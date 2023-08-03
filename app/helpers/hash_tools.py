3e2848213d515940bee235f0d36b1fefc5c7809b
akhomlyuk <relicbs@gmail.com>
2023-08-04 00:24:12 +0300

    v0.3 added Hash type identifier

--- app/helpers/hash_tools.py
+++ app/helpers/hash_tools.py
@@ -4,11 +4,12 @@ from icecream import ic
 
 def hash_analyze(hash_string: str):
     try:
-        if not runner.api_return_hashes_as_dict([hash_string], {"popular_only": True}):
-            text = runner.api_return_hashes_as_dict([hash_string], {"popular_only": True})
+        if runner.api_return_hashes_as_dict([hash_string], {"popular_only": True}) == {hash_string: []}:
+            text = runner.api_return_hashes_as_dict([hash_string])
             return text[hash_string]
         else:
-            text = runner.api_return_hashes_as_dict([hash_string])
+            text = runner.api_return_hashes_as_dict([hash_string], {"popular_only": True})
+
             return text[hash_string]
     except Exception as e:
         ic(e)
