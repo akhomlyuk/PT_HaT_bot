ccf9be8becdade9518c95fe6c0990c682746fe34
akhomlyuk <relicbs@gmail.com>
2023-08-04 00:04:23 +0300

    v0.3 added Hash type identifier

--- app/helpers/hash_tools.py
+++ app/helpers/hash_tools.py
@@ -6,17 +6,9 @@ def hash_analyze(hash_string: str):
     try:
         if not runner.api_return_hashes_as_dict([hash_string], {"popular_only": True}):
             text = runner.api_return_hashes_as_dict([hash_string], {"popular_only": True})
-            ic(len(text[hash_string]))
-            ic(text[hash_string])
-            ic(hash_string)
-            ic()
             return text[hash_string]
         else:
             text = runner.api_return_hashes_as_dict([hash_string])
-            ic(len(text[hash_string]))
-            ic(text[hash_string])
-            ic(hash_string)
-            ic()
             return text[hash_string]
     except Exception as e:
         ic(e)
