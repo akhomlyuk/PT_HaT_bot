2cb960fb5c30047441d62af7be21358d0fec63df
akhomlyuk <relicbs@gmail.com>
2023-08-05 18:39:10 +0300

    v0.3 added Hash type identifier

--- app/handlers/free_gpt.py
+++ app/handlers/free_gpt.py
@@ -15,6 +15,7 @@ def get_free_gpt_list():
         r = requests.get('https://raw.githubusercontent.com/LiLittleCat/awesome-free-chatgpt/main/urls.json')
         gpt_list = json.loads(r.text)
         randomized_urls = random.sample(gpt_list, 10)
+        json_data = json.dumps(randomized_urls, indent=2)
         return randomized_urls
     except Exception as e:
         ic(e)
@@ -25,7 +26,8 @@ def get_free_gpt_list():
 @router.message(F.text.in_(cfg.all_commands['gpt_cmds']))
 async def send_freegpt_list(message: Message):
     try:
-        await message.answer(f"{get_free_gpt_list()}")
+        gpt_list = "\n".join(str(i) for i in get_free_gpt_list())
+        await message.answer(f'Список случайных GPT ботов:\n{gpt_list}', disable_web_page_preview=True)
     except Exception as e:
         logging.warning(e)
         ic(e)
