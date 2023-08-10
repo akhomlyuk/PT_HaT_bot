a9a0b96c57240a708ee57be5413314d5c4f8eeed
akhomlyuk <relicbs@gmail.com>
2023-08-10 19:45:39 +0300

    v0.4 added sqli gen payloads, chatgpt list

--- app/handlers/searchsploit.py
+++ app/handlers/searchsploit.py
@@ -4,6 +4,8 @@ import logging
 import app.config.cfg as cfg
 from icecream import ic
 import os
+import subprocess
+import json
 
 router: Router = Router()
 
@@ -12,10 +14,17 @@ router: Router = Router()
 async def search_sploit(message: Message):
     try:
         msg = message.text.split()
-        sploit_string = ' '.join(msg[1])
-        ss = os.popen('searchsploit Microsoft Exchange 2019 -j -w').read()[:-1]
-        ic(ss)
-        await message.answer(f'{ss}')
+        if len(msg) == 1:
+            await message.answer('!ss Microsoft Exchange 2019')
+        else:
+            sploit_string = ' '.join(msg[1])
+            command = f"searchsploit {sploit_string} -j -w"
+            output_file = "sploit.json"
+            subprocess.run(f"{command} > {output_file}", shell=True)
+            with open(output_file, "r") as file:
+                data = json.load(file)
+            ic(data)
+            await message.answer(f'{data}')
     except Exception as e:
         ic()
         ic(e)
