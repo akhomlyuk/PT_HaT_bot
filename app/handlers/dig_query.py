a6a885c5ddb4dd005e8e689f69ef480f6a24c8b9
akhomlyuk <relicbs@gmail.com>
2023-08-13 15:46:52 +0300

    v0.4 added port checking, whois info, dig query

--- app/handlers/dig_query.py
+++ app/handlers/dig_query.py
@@ -15,9 +15,19 @@ async def dig_query(message: Message):
     try:
         msg = message.text.split()
         if len(msg) == 1:
-            await message.answer('<code>!dig ya.ru (A,MX,TXT,etc)</code>\n\nhttps://en.wikipedia.org/wiki/List_of_DNS_record_types', disable_web_page_preview=True)
+            await message.answer('<code>!dig ya.ru</code>\nor\n<code>!dig ya.ru (A,MX,TXT,etc)</code>\n\nhttps://en.wikipedia.org/wiki/List_of_DNS_record_types', disable_web_page_preview=True)
+        elif len(msg) == 2:
+            query_a = pydig.query(msg[1], 'A')
+            query_cname = pydig.query(msg[1], 'CNAME')
+            query_mx = pydig.query(msg[1], 'MX')
+            query_ns = pydig.query(msg[1], 'NS')
+            query_ptr = pydig.query(msg[1], 'PTR')
+            query_soa = pydig.query(msg[1], 'SOA')
+            query_txt = pydig.query(msg[1], 'TXT')
+            await message.answer(f"<code>{query_a}</code>\n<code>{query_cname}</code>\n<code>{query_mx}</code>\n<code>{query_ns}</code>"
+                                 f"\n<code>{query_ptr}</code>\n<code>{query_soa}</code>\n<code>{query_txt}</code>")
         elif len(msg) != 3:
-            await message.answer('<code>!dig ya.ru (A,MX,TXT,etc)</code>\n\nhttps://en.wikipedia.org/wiki/List_of_DNS_record_types', disable_web_page_preview=True)
+            await message.answer('<code>!dig ya.ru</code>\nor\n<code>!dig ya.ru (A,MX,TXT,etc)</code>\n\nhttps://en.wikipedia.org/wiki/List_of_DNS_record_types', disable_web_page_preview=True)
         else:
             query = pydig.query(msg[1], msg[2])
             await message.answer(f"<code>{query}</code>")
