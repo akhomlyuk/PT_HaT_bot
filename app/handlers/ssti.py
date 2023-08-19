d738d294d45f0f1d05c82bca0f8ef8eda45f6573
akhomlyuk <relicbs@gmail.com>
2023-08-19 20:25:26 +0300

    v0.4 added port checking, whois info, dig query

--- app/handlers/ssti.py
+++ app/handlers/ssti.py
@@ -12,7 +12,8 @@ async def send_ssti_identify(message: Message):
     try:
         msg = message.text.split()
         if len(msg) == 1:
-            await message.answer_photo('AgACAgIAAxkBAAIF_mTeHJWrQxmkISotQYadLk-iOW_qAAL70zEbb7LwSqnZnI0KVt_UAQADAgADeAADMAQ',
+            photo = URLInputFile('https://1517081779-files.gitbook.io/~/files/v0/b/gitbook-legacy-files/o/assets%2F-L_2uGJGU7AVNRcqRvEi%2F-M7O4Hp6bOFFkge_yq4G%2F-M7OCvxwZCiaP8Whx2fi%2Fimage.png?alt=media&token=4b40cf58-5561-4925-bc86-1d4689ca53d1')
+            await message.answer_photo(photo,
                                        caption='\nhttps://book.hacktricks.xyz/pentesting-web/ssti-server-side-template-injection')
         else:
             msg2 = message.text[5:]
