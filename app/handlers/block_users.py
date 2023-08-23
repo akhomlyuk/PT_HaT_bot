5b297251f84c5ba95ad2dec851ddf9229aa29e50
akhomlyuk <relicbs@gmail.com>
2023-08-23 12:45:02 +0300

    v0.5 ban/unban cmd, links, fixes

--- app/handlers/block_users.py
+++ app/handlers/block_users.py
@@ -11,7 +11,7 @@ router: Router = Router()
 @router.message(F.text.startswith(cfg.all_commands['ban_cmds']))
 async def ban_user(message: Message):
     try:
-        if message.message_id in cfg.admins:
+        if message.from_user.id in cfg.admins:
             await message.bot.ban_chat_member(message.chat.id, message.reply_to_message.from_user.id)
             await message.answer(f'Пользователь {message.from_user.first_name} заблокирован!')
         else:
