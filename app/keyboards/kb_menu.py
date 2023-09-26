05e696cb09b5744ac1370ea063a3f179b73abc84
akhomlyuk <relicbs@gmail.com>
2023-09-26 20:09:27 +0300

    v0.6 add proxy_checker, code refactoring

--- app/keyboards/kb_menu.py
+++ app/keyboards/kb_menu.py
@@ -52,13 +52,13 @@ async def menu_buttons(message: Message):
 # Коллбеки для пунктов меню
 @router.callback_query(F.data == 'rht_info')
 async def send_rht_info(callback: CallbackQuery):
-    await callback.message.answer(f'{description.show_proxies_help}')
+    await callback.message.answer(f'{rht_summary}')
     await callback.answer()
 
 
 @router.callback_query(F.data == 'free_proxy')
 async def send_proxy_info(callback: CallbackQuery):
-    await callback.message.answer(f'{rht_summary}')
+    await callback.message.answer(f'{description.show_proxies_help}')
     await callback.answer()
 
 
