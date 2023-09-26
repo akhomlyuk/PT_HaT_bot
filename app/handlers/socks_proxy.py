c78dfcb547077923f560f76fd927cb27a663b74e
akhomlyuk <relicbs@gmail.com>
2023-09-26 23:30:06 +0300

    v0.6 add proxy_checker, code refactoring

--- app/handlers/socks_proxy.py
+++ app/handlers/socks_proxy.py
@@ -9,68 +9,53 @@ from app.texts import description
 
 router: Router = Router()
 
+file_types = ['socks5', 'http', 'socks4']
+geo_file_types = ['socks5', 'http', 'socks4']
+
 proxies_path = os.path.expanduser('~')
 
-if platform == "win32":
-    with open(proxies_path + r'\proxies\proxies\socks5.txt', 'r') as socks5_file:
-        socks5_file = socks5_file.read()
-    with open(proxies_path + r'\proxies\proxies\http.txt', 'r') as http_file:
-        http_file = http_file.read()
-    with open(proxies_path + r'\proxies\proxies\socks4.txt', 'r') as socks4_file:
-        socks4_file = socks4_file.read()
-else:
-    with open(proxies_path + r'/proxies/proxies/socks5.txt', 'r') as socks5_file:
-        socks5_file = socks5_file.read()
-    with open(proxies_path + r'/proxies/proxies/http.txt', 'r') as http_file:
-        http_file = http_file.read()
-    with open(proxies_path + r'/proxies/proxies/socks4.txt', 'r') as socks4_file:
-        socks4_file = socks4_file.read()
+file_paths = {}
+geo_file_paths = {}
 
 if platform == "win32":
-    with open(proxies_path + r'\proxies\proxies_geolocation\socks5.txt', 'r') as socks5_file_geo:
-        socks5_file_geo = socks5_file_geo.read()
-    with open(proxies_path + r'\proxies\proxies_geolocation\http.txt', 'r') as http_file_geo:
-        http_file_geo = http_file_geo.read()
-    with open(proxies_path + r'\proxies\proxies_geolocation\socks4.txt', 'r') as socks4_file_geo:
-        socks4_file_geo = socks4_file_geo.read()
+    path_separator = '\\'
+    proxies_dir = 'proxies' + path_separator + 'proxies'
+    geo_dir = 'proxies' + path_separator + 'proxies_geolocation'
 else:
-    with open(proxies_path + r'/proxies/proxies_geolocation/socks5.txt', 'r') as socks5_file_geo:
-        socks5_file_geo = socks5_file_geo.read()
-    with open(proxies_path + r'/proxies/proxies_geolocation/http.txt', 'r') as http_file_geo:
-        http_file_geo = http_file_geo.read()
-    with open(proxies_path + r'/proxies/proxies_geolocation/socks4.txt', 'r') as socks4_file_geo:
-        socks4_file_geo = socks4_file_geo.read()
+    path_separator = '/'
+    proxies_dir = 'proxies' + path_separator + 'proxies'
+    geo_dir = 'proxies' + path_separator + 'proxies_geolocation'
 
-socks5_lst = list(socks5_file.split('\n'))
-random_socks5 = random.choices(socks5_lst, k=3)
+for file_type in file_types:
+    file_path = os.path.join(proxies_path, proxies_dir, file_type + '.txt')
+    with open(file_path, 'r') as file:
+        file_paths[file_type] = file.read().split('\n')
 
-socks5_lst_geo = list(socks5_file_geo.split('\n'))
-random_socks5_geo = random.choices(socks5_lst_geo, k=3)
+for file_type in geo_file_types:
+    file_path = os.path.join(proxies_path, geo_dir, file_type + '.txt')
+    with open(file_path, 'r') as file:
+        geo_file_paths[file_type] = file.read().split('\n')
 
-socks4_lst = list(socks4_file.split('\n'))
-random_socks4 = random.choices(socks4_lst, k=3)
+random_socks5 = random.choices(file_paths['socks5'], k=3)
+random_socks4 = random.choices(file_paths['socks4'], k=3)
+random_http = random.choices(file_paths['http'], k=3)
 
-socks4_lst_geo = list(socks4_file_geo.split('\n'))
-random_socks4_geo = random.choices(socks4_lst_geo, k=3)
-
-http_lst = list(http_file.split('\n'))
-random_http = random.choices(http_lst, k=3)
-
-http_lst_geo = list(http_file_geo.split('\n'))
-random_http_geo = random.choices(http_lst_geo, k=3)
+random_socks5_geo = random.choices(geo_file_paths['socks5'], k=3)
+random_socks4_geo = random.choices(geo_file_paths['socks4'], k=3)
+random_http_geo = random.choices(geo_file_paths['http'], k=3)
 
 
 @router.message(F.text.startswith(cfg.all_commands['socks5_cmds']))
 async def get_socks5_proxy(message: Message):
     try:
         args = message.text.split()
-        newline = '\n'
+        nr = '\n'
         if len(args) == 1:
-            await message.answer(f"<b>Socks5 proxies:</b>\n<code>{newline.join(random_socks5)}</code>")
+            await message.answer(f"<b>Socks5 proxies:</b>\n<code>{nr.join(random_socks5)}</code>")
         elif len(args) == 2:
             try:
                 if 1 <= int(args[1]) <= 20:
-                    await message.answer(f"<b>Socks5 proxies:</b>\n<code>{newline.join(random.choices(socks5_lst, k=int(args[1])))}</code>")
+                    await message.answer(f"<b>Socks5 proxies:</b>\n<code>{nr.join(random.choices(file_paths['socks5'], k=int(args[1])))}</code>")
                 else:
                     await message.answer(f"<b>Usage:</b>\n<code>!socks5 1...20</code>\n<code>!socks5 1...20 geo</code>")
             except ValueError:
@@ -78,7 +63,7 @@ async def get_socks5_proxy(message: Message):
         elif len(args) == 3:
             if args[2] == "geo" and 1 <= int(args[1]) <= 20:
                 await message.answer(f"<b>Socks5 proxies with geo:</b>\n"
-                                     f"<code>{newline.join(random.choices(socks5_lst_geo, k=int(args[1])))}</code>")
+                                     f"<code>{nr.join(random.choices(geo_file_paths['socks5'], k=int(args[1])))}</code>")
             else:
                 await message.answer(f"<b>Usage:</b>\n<code>!socks5 1...20 geo</code>")
     except Exception as e:
@@ -90,13 +75,13 @@ async def get_socks5_proxy(message: Message):
 async def get_socks4_proxy(message: Message):
     try:
         args = message.text.split()
-        newline = '\n'
+        nr = '\n'
         if len(args) == 1:
-            await message.answer(f"<b>Socks4 proxies:</b>\n<code>{newline.join(random_socks4)}</code>")
+            await message.answer(f"<b>Socks4 proxies:</b>\n<code>{nr.join(random_socks4)}</code>")
         elif len(args) == 2:
             try:
                 if 1 <= int(args[1]) <= 20:
-                    await message.answer(f"<b>Socks4 proxies:</b>\n<code>{newline.join(random.choices(socks4_lst, k=int(args[1])))}</code>")
+                    await message.answer(f"<b>Socks4 proxies:</b>\n<code>{nr.join(random.choices(file_paths['socks4'], k=int(args[1])))}</code>")
                 else:
                     await message.answer(f"<b>Usage:</b>\n<code>!socks4 1...20</code>")
             except ValueError:
@@ -104,7 +89,7 @@ async def get_socks4_proxy(message: Message):
         elif len(args) == 3:
             if args[2] == "geo" and 1 <= int(args[1]) <= 20:
                 await message.answer(f"<b>Socks4 proxies with geo:</b>\n"
-                                     f"<code>{newline.join(random.choices(socks4_lst_geo, k=int(args[1])))}</code>")
+                                     f"<code>{nr.join(random.choices(geo_file_paths['socks4'], k=int(args[1])))}</code>")
             else:
                 await message.answer(f"<b>Usage:</b>\n<code>!socks4 1...20 geo</code>")
 
@@ -117,13 +102,13 @@ async def get_socks4_proxy(message: Message):
 async def get_http_proxy(message: Message):
     try:
         args = message.text.split()
-        newline = '\n'
+        nr = '\n'
         if len(args) == 1:
-            await message.answer(f"<b>Http proxies:</b>\n<code>{newline.join(random_http)}</code>")
+            await message.answer(f"<b>Http proxies:</b>\n<code>{nr.join(random_http)}</code>")
         elif len(args) == 2:
             try:
                 if 1 <= int(args[1]) <= 20:
-                    await message.answer(f"<b>Http proxies:</b>\n<code>{newline.join(random.choices(http_lst, k=int(args[1])))}</code>")
+                    await message.answer(f"<b>Http proxies:</b>\n<code>{nr.join(random.choices(file_paths['http'], k=int(args[1])))}</code>")
                 else:
                     await message.answer(f"<b>Usage:</b>\n<code>!http 1...20</code>")
             except ValueError:
@@ -131,7 +116,7 @@ async def get_http_proxy(message: Message):
         elif len(args) == 3:
             if args[2] == "geo" and 1 <= int(args[1]) <= 20:
                 await message.answer(f"<b>Http proxies with geo:</b>\n"
-                                     f"<code>{newline.join(random.choices(http_lst_geo, k=int(args[1])))}</code>")
+                                     f"<code>{nr.join(random.choices(geo_file_paths['http'], k=int(args[1])))}</code>")
             else:
                 await message.answer(f"<b>Usage:</b>\n<code>!http 1...20 geo</code>")
 
@@ -143,9 +128,9 @@ async def get_http_proxy(message: Message):
 @router.message(F.text.startswith(cfg.all_commands['proxies_cmds']))
 async def get_proxies_help(message: Message):
     try:
-        await message.answer(f"<b>Socks5</b>: {len(socks5_lst)}\n"
-                             f"<b>Socks4</b>: {len(socks4_lst)}\n"
-                             f"<b>Http</b>: {len(http_lst)}\n\n"
+        await message.answer(f"<b>Socks5</b>: {len(file_paths['socks5'])}\n"
+                             f"<b>Socks4</b>: {len(file_paths['socks4'])}\n"
+                             f"<b>Http</b>: {len(file_paths['http'])}\n\n"
                              f"{description.show_proxies_help}")
     except Exception as e:
         ic()
