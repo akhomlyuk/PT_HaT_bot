3ef41b0367d4258fc1006d31ddf76cc634fe7fc8
akhomlyuk <relicbs@gmail.com>
2023-11-20 13:39:39 +0300

    v0.6a added lxlm support, some code refactoring

--- app/handlers/ctftime_handler.py
+++ app/handlers/ctftime_handler.py
@@ -44,7 +44,7 @@ async def top_teams_ru(message: Message):
 @router.message(F.text.startswith(cfg.all_commands['bestres_cmds']))
 async def rhteam_best(message: Message):
     try:
-        await message.answer(f'Organized CTF events\nCybercoliseum: {22.83 * 2}\nCybercoliseum II: {20.60 * 2}'
+        await message.answer(f'Organized CTF events\nCybercoliseum: {22.83 * 2}\nCybercoliseum II: {20.60 * 2}\n'
                              f'<b>Лучшие результаты по рейтингу</b>\n\n{top10_results}', disable_web_page_preview=True)
     except Exception as e:
         ic()
