433109ce1660d6aea36e297465d0dc2e719467c9
akhomlyuk <relicbs@gmail.com>
2024-03-12 13:54:50 +0300

    added obfs4 bridges for tor

--- app/handlers/get_obfs4_tor_bridges.py
+++ app/handlers/get_obfs4_tor_bridges.py
@@ -18,8 +18,8 @@ rnd_bridges = random.choices(bridges, k=4)
 @router.message(F.text.in_(cfg.all_commands['bridges']))
 async def send_tor_bridges_list(message: Message):
     try:
-        bridges_list = "\n".join(str(i) for i in rnd_bridges)
-        await message.answer(f'Мосты(obfs4 bridges) для Tor:\n\n{bridges_list}', disable_web_page_preview=True)
+        bridges_list = "\n\n".join(str(i) for i in rnd_bridges)
+        await message.answer(f'Мосты(obfs4 bridges) для Tor:\n\n```{bridges_list}```', disable_web_page_preview=True)
     except Exception as e:
         logging.warning(e)
         ic(e)
