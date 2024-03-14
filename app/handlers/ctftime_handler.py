74f4c78c71b0b50ce8403f2c839180f9ef56edca
akhomlyuk <relicbs@gmail.com>
2024-03-14 14:26:31 +0300

    added feedback link

--- app/handlers/ctftime_handler.py
+++ app/handlers/ctftime_handler.py
@@ -7,28 +7,28 @@ from app.helpers.ctftime_parser import rht_info, rht_best_res, top_teams_ru
 
 router: Router = Router()
 
-rht_info = rht_info()
+# rht_info = rht_info()
 rht_best = rht_best_res()
 top_ru = top_teams_ru()
 
-rht_summary = f'''🌍 Worldwide position: <b>{rht_info["rating"]["2024"]["rating_place"]}</b>
-🇷🇺 RU position: <b>{rht_info["rating"]["2024"]["country_place"]}</b>
-🎯 Rating points: <b>{rht_info["rating"]["2024"]["rating_points"]}</b>
-🚩 Team ID: <b>{rht_info["id"]}</b>
-https://ctftime.org/team/186788'''
-
-top10_results = '\n'.join([i for i in rht_best[2]])
-
-
-@router.message(F.text.startswith(cfg.all_commands['team_cmds']))
-async def rhteam_info(message: Message):
-    try:
-        await message.answer(f'{rht_summary}')
-    except Exception as e:
-        ic()
-        ic(e)
-        logging.error(e)
-        pass
+# rht_summary = f'''🌍 Worldwide position: <b>{rht_info["rating"]["2024"]["rating_place"]}</b>
+# 🇷🇺 RU position: <b>{rht_info["rating"]["2024"]["country_place"]}</b>
+# 🎯 Rating points: <b>{rht_info["rating"]["2024"]["rating_points"]}</b>
+# 🚩 Team ID: <b>{rht_info["id"]}</b>
+# https://ctftime.org/team/186788'''
+#
+# top10_results = '\n'.join([i for i in rht_best[2]])
+#
+#
+# @router.message(F.text.startswith(cfg.all_commands['team_cmds']))
+# async def rhteam_info(message: Message):
+#     try:
+#         await message.answer(f'{rht_summary}')
+#     except Exception as e:
+#         ic()
+#         ic(e)
+#         logging.error(e)
+#         pass
 
 
 @router.message(F.text.startswith(cfg.all_commands['topteams_cmds']))
