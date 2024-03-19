bb5b34aed0314ab9ac2ed5365567f8a5015efb31
akhomlyuk <relicbs@gmail.com>
2024-03-19 10:47:33 +0300

    api changed

--- app/keyboards/kb_menu.py
+++ app/keyboards/kb_menu.py
@@ -7,7 +7,7 @@ from app.config import cfg
 from app.config.cfg import bot
 from app.texts import revshells, description
 from app.texts.sqli_examples import sqli_example
-from app.helpers.ctftime_parser import top_teams_ru, rht_info
+from app.helpers.ctftime_parser import rht_info
 from app.handlers.ctftime_handler import rht_summary, top10_results
 from icecream import ic
 
